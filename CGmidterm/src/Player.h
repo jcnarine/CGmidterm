a3a5732a7c220ce8dae584d6347fce4da3d5182f
Jonathan <j.c.narine@gmail.com>
2020-10-31 21:27:22 -0500

    Changed Location of Code

---
+++ CGmidterm/src/Player.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <memory>
+#include <GLM/glm.hpp>
+#include <string>
+using namespace std;
+
+
+class Player
+{
+
+public:
+
+
+	Player();
+   ~Player();
+
+
+
+
+
+private:
+
+
+
+
+
+
+
+};
+
